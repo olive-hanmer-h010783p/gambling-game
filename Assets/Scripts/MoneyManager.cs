87e5c2df2bc749d7cb874c326f7b4f2e5897ce3e
Chris-Jones135 <j015066p@student.staffs.ac.uk>
2026-01-09 01:31:16 +0000

    Lose Screen and shell of the MoneyManager script

---
+++ Assets/Scripts/MoneyManager.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class MoneyManager : MonoBehaviour
+{
+    [SerializeField] private int maxChips;
+    public int remainingChips;
+    public int playersChips;
+    public int bet;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Bet()
+    {
+
+    }
+}
